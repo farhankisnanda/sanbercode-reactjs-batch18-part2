246b1b87af755f543955d22471d83e88d56fcaeb
farhankisnanda <kingkiss2901@gmail.com>
2020-10-09 01:54:55 +0700

    Menyelesaikan Tugas React Js Pertama

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width:'80%',height:'500px',border:'5px solid black',color:'black',margin:'40px auto'}}>
+        <h1 style={{margin:'30px auto'}}>Form Pembelian Buah</h1>
+        <form style={{width:'50%',float:"left",}}>
+          <h2>Nama Pelanggan</h2>
+          <h2 style={{marginTop:'200px',marginLeft:'-65px'}}>Daftar Item</h2>
+        </form>
+        <form>
+        <input style={{boxSizing:'border-box',width:'30%', margin:'25px 0px',marginLeft:'-400px'}}></input>
+        <form>
+        <input style={{boxSizing:'border-box',width:'20px', margin:'25px 0px',marginLeft:'-700px',marginTop:'-100px'}}></input>
+        <br></br>
+        <input style={{boxSizing:'border-box',width:'20px', margin:'25px 0px',marginLeft:'-700px',marginTop:'-100px'}}></input>
+        <br></br>
+        <input style={{boxSizing:'border-box',width:'20px', margin:'25px 0px',marginLeft:'-700px',marginTop:'-100px'}}></input>
+        <br></br>
+        <input style={{boxSizing:'border-box',width:'20px', margin:'25px 0px',marginLeft:'-700px',marginTop:'-100px'}}></input>
+        <br></br>
+        <input style={{boxSizing:'border-box',width:'20px', margin:'25px 0px',marginLeft:'-700px',marginTop:'-100px'}}></input>
+        </form>
+        <h3 style={{float:'right',marginTop:'-230px',marginRight:'500px'}}>Semangka</h3>
+        <h3 style={{float:'right',marginTop:'-185px',marginRight:'500px'}}>Semangka</h3>
+        <h3 style={{float:'right',marginTop:'-138px',marginRight:'500px'}}>Semangka</h3>
+        <h3 style={{float:'right',marginTop:'-92px',marginRight:'500px'}}>Semangka</h3>
+        <h3 style={{float:'right',marginTop:'-45px',marginRight:'500px'}}>Semangka</h3>
+        </form>
+        <button type='submit' style={{padding:'5px',margin:'20px',marginLeft:'-650px'}}>Kirim</button>
     </div>
   );
 }
-
 export default App;
